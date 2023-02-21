255f8d0d019ad105a9f5c97f8214a937b251efef
Arshad Kazmi <arshadkazmi42@gmail.com>
2023-02-21 12:22:44 +0100

    Remove events.js from index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,4 @@
 export { default as COLORS } from './colors';
 export { default as TOOLS } from './tools';
-export { default as EVENTS } from './events';
 export { default as LANGUAGE } from './language';
 export { default as STATES } from './states';
